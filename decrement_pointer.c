a488763a9b1d665941db000050992c5fa0c6de16
Shivangi <shivangi639661@gmail.com>
2024-04-24 09:32:15 -0700

    added

---
+++ decrement_pointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ void main()
+ {
+    int number = 50;
+    int*p;
+    p = &number;
+    printf("Addressof p variable is %u\n",p);
+    p = p-1;
+    printf("After decrement:Address of p variable is %u\n",p);
+    
+ }
