4a7efacd98d99427356a60f8428b8e0629d4c2c9
Shivangi <159949628+codershivangi@users.noreply.github.com>
2024-10-19 08:17:38 -0700

    Add files via upload

---
+++ percentage.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main()
+ {
+    float m1 = 65;
+    float m2 = 78;
+    float m3 = 98;
+    float m4 = 89;
+    float m5 = 90;
+    float p = (m1+m2+m3+m4+m5)/5;
+    printf("pecentage is:%f",p);
+    return 0;
+ }
